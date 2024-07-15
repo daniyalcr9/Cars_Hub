36ce8b5f34f93900df1eec02a9325a6604e7c869
Daniyal Awan <daniyalawancr9@gmail.com>
2024-07-15 15:24:06 +0500

    6th commit

--- cars_hub/lib/home.dart
+++ cars_hub/lib/home.dart
@@ -1,3 +1,6 @@
+import 'package:cars_hub/home_main.dart';
+import 'package:cars_hub/infohub.dart';
+import 'package:cars_hub/otherservices.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -10,260 +13,69 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Center(
-          child: Text(
-            'Cars Hub',
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 28,
-              color: Color.fromARGB(255, 32, 122, 182),
-            ),
-          ),
-        ),
-        leading: Icon(Icons.menu),
-        actions: [
-          Padding(
-            padding: const EdgeInsets.only(right: 8),
-            child: Icon(Icons.person),
-          )
-        ],
-      ),
-      backgroundColor: Colors.transparent, // Setting transparent background
-      extendBodyBehindAppBar: true, // Extending background behind the app bar
-      body: ListView.builder(
-        padding: EdgeInsets.zero, // Remove padding
-        itemCount: 1, // Number of cars
-        itemBuilder: (context, index) {
-          return Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [
-                  Color.fromARGB(255, 32, 122, 182),
-                  Color.fromARGB(255, 7, 6, 110),
-                  Color.fromARGB(255, 32, 122, 182),
-                  Color.fromARGB(255, 7, 6, 110),
-                  Color.fromARGB(255, 32, 122, 182),
-                ], // Mixture of blue and green
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: Text(
+              'Cars Hub',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 28,
+                color: Color.fromARGB(255, 32, 122, 182),
               ),
             ),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 100, horizontal: 8),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  TextField(
-                    style: TextStyle(fontSize: 14), // Adjust text size
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.white,
-                      contentPadding: EdgeInsets.symmetric(
-                        vertical: 10,
-                        horizontal: 15,
-                      ), // Adjust padding
-                      hintText: 'Search any Car of your choice!',
-                      prefixIcon: Icon(Icons.search),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(
-                          20.0,
-                        ), // Making search bar circular
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 20),
-                  Text(
-                    "Buy a Car Today!",
-                    style: TextStyle(
-                        fontSize: 50,
-                        fontWeight: FontWeight.bold,
-                        color: Color.fromARGB(255, 165, 193, 212)),
-                  ),
-                  SizedBox(height: 8),
-                  Card(
-                    elevation: 0,
-                    child: Column(
-                      children: [
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(
-                            8.0,
-                          ), // Rounded corners for image
-                          child: Image.asset(
-                            'assets/car1.jpg',
-                            height: 150,
-                            width: double.infinity,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                        SizedBox(height: 8),
-                        Text(
-                          'COROLLA',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Color.fromARGB(255, 7, 6, 110),
-                          ),
-                        ),
-                        SizedBox(height: 4),
-                        Text(
-                          'Make: Toyota  Model: 2018  Variant: Grande 1.8L',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 13,
-                            color: Color.fromARGB(255, 7, 6, 110),
-                          ),
-                        ),
-                        SizedBox(height: 8),
-                      ],
-                    ),
-                  ),
-                  SizedBox(
-                    height: 8,
-                  ),
-                  Card(
-                    elevation: 5,
-                    child: Column(
-                      children: [
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(
-                            8.0,
-                          ), // Rounded corners for image
-                          child: Image.asset(
-                            'assets/secondcar.jpg',
-                            height: 150,
-                            width: double.infinity,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                        SizedBox(height: 8),
-                        Text(
-                          'CIVIC',
-                          style: TextStyle(
-                              color: Color.fromARGB(255, 7, 6, 110),
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20),
-                        ),
-                        SizedBox(height: 4),
-                        Text(
-                          'Make: Honda | Model: 2023 | Variant: Oriel 1.8L',
-                          style: TextStyle(
-                              color: Color.fromARGB(255, 7, 6, 110),
-                              fontWeight: FontWeight.bold,
-                              fontSize: 13),
-                        ),
-                        SizedBox(height: 8),
-                      ],
-                    ),
-                  ),
-                  SizedBox(
-                    height: 8,
-                  ),
-                  Card(
-                    elevation: 5,
-                    child: Column(
-                      children: [
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(
-                            8.0,
-                          ), // Rounded corners for image
-                          child: Image.asset(
-                            'assets/car3.jpg',
-                            height: 150,
-                            width: double.infinity,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                        SizedBox(height: 8),
-                        Text(
-                          'SONATA',
-                          style: TextStyle(
-                              color: Color.fromARGB(255, 7, 6, 110),
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20),
-                        ),
-                        SizedBox(height: 4),
-                        Text(
-                          'Make: Hyundai  Model: 2024  Variant: Grande 2.3L',
-                          style: TextStyle(
-                              color: Color.fromARGB(255, 7, 6, 110),
-                              fontWeight: FontWeight.bold,
-                              fontSize: 13),
-                        ),
-                        SizedBox(height: 8),
-                      ],
-                    ),
-                  ),
-                  SizedBox(
-                    height: 8,
-                  ),
-                  Card(
-                    elevation: 5,
-                    child: Column(
-                      children: [
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(
-                            8.0,
-                          ), // Rounded corners for image
-                          child: Image.asset(
-                            'assets/secondcar.jpg',
-                            height: 150,
-                            width: double.infinity,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                        SizedBox(height: 8),
-                        Text(
-                          'COROLLA',
-                          style: TextStyle(
-                              color: Color.fromARGB(255, 7, 6, 110),
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20),
-                        ),
-                        SizedBox(height: 4),
-                        Text(
-                          'Make: Toyota | Model: 2018 | Variant: Grande 1.8L',
-                          style: TextStyle(
-                              color: Color.fromARGB(255, 7, 6, 110),
-                              fontWeight: FontWeight.bold,
-                              fontSize: 13),
-                        ),
-                        SizedBox(height: 8),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // Add functionality for the floating button
-        },
-        child: Icon(Icons.add),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(30.0), // Make it circular
+          ),
+          leading: Icon(Icons.menu),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 8),
+              child: Icon(Icons.person),
+            )
+          ],
         ),
-        // backgroundColor: Theme.of(context).accentColor, // Using accent color for the floating action button
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
+        backgroundColor: Colors.transparent, // Setting transparent background
+        extendBodyBehindAppBar: true, // Extending background behind the app bar
+        body: TabBarView(
+          children: [MyWidget(), infohub(), otherservices()],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            // Add functionality for the floating button
+          },
+          child: Icon(Icons.add),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30.0), // Make it circular
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.info),
-            label: 'Info Hub',
+          // backgroundColor: Theme.of(context).accentColor, // Using accent color for the floating action button
+        ),
+        bottomNavigationBar: Container(
+          child: TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.home), text: "Home"),
+              Tab(icon: Icon(Icons.home), text: "Home"),
+              Tab(icon: Icon(Icons.home), text: "Home"),
+            ],
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: 'Other Services',
-          ),
-        ],
+        ),
+        //BottomNavigationBar(
+        //   items: [
+        //     BottomNavigationBarItem(
+        //       Container,
+        //       icon: Icon(Icons.home),
+        //       label: 'Home',
+        //     ),
+        //     BottomNavigationBarItem(
+        //       icon: Icon(Icons.info),
+        //       label: 'Info Hub',
+        //     ),
+        //     BottomNavigationBarItem(
+        //       icon: Icon(Icons.settings),
+        //       label: 'Other Services',
+        //     ),
+        //   ],
+        // ),
       ),
     );
   }
---
+++ cars_hub/lib/home_main.dart
@@ -0,0 +1,222 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: EdgeInsets.zero, // Remove padding
+      itemCount: 1, // Number of cars
+      itemBuilder: (context, index) {
+        return Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Color.fromARGB(255, 32, 122, 182),
+                Color.fromARGB(255, 7, 6, 110),
+                Color.fromARGB(255, 32, 122, 182),
+                Color.fromARGB(255, 7, 6, 110),
+                Color.fromARGB(255, 32, 122, 182),
+              ], // Mixture of blue and green
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 100, horizontal: 8),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                TextField(
+                  style: TextStyle(fontSize: 14), // Adjust text size
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    contentPadding: EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 15,
+                    ), // Adjust padding
+                    hintText: 'Search any Car of your choice!',
+                    prefixIcon: Icon(Icons.search),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(
+                        20.0,
+                      ), // Making search bar circular
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  "Buy a Car Today!",
+                  style: TextStyle(
+                      fontSize: 50,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 165, 193, 212)),
+                ),
+                SizedBox(height: 8),
+                Card(
+                  elevation: 0,
+                  child: Column(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(
+                          8.0,
+                        ), // Rounded corners for image
+                        child: Image.asset(
+                          'assets/car1.jpg',
+                          height: 150,
+                          width: double.infinity,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        'COROLLA',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Color.fromARGB(255, 7, 6, 110),
+                        ),
+                      ),
+                      SizedBox(height: 4),
+                      Text(
+                        'Make: Toyota  Model: 2018  Variant: Grande 1.8L',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 13,
+                          color: Color.fromARGB(255, 7, 6, 110),
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                Card(
+                  elevation: 5,
+                  child: Column(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(
+                          8.0,
+                        ), // Rounded corners for image
+                        child: Image.asset(
+                          'assets/secondcar.jpg',
+                          height: 150,
+                          width: double.infinity,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        'CIVIC',
+                        style: TextStyle(
+                            color: Color.fromARGB(255, 7, 6, 110),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20),
+                      ),
+                      SizedBox(height: 4),
+                      Text(
+                        'Make: Honda | Model: 2023 | Variant: Oriel 1.8L',
+                        style: TextStyle(
+                            color: Color.fromARGB(255, 7, 6, 110),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 13),
+                      ),
+                      SizedBox(height: 8),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                Card(
+                  elevation: 5,
+                  child: Column(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(
+                          8.0,
+                        ), // Rounded corners for image
+                        child: Image.asset(
+                          'assets/car3.jpg',
+                          height: 150,
+                          width: double.infinity,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        'SONATA',
+                        style: TextStyle(
+                            color: Color.fromARGB(255, 7, 6, 110),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20),
+                      ),
+                      SizedBox(height: 4),
+                      Text(
+                        'Make: Hyundai  Model: 2024  Variant: Grande 2.3L',
+                        style: TextStyle(
+                            color: Color.fromARGB(255, 7, 6, 110),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 13),
+                      ),
+                      SizedBox(height: 8),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                Card(
+                  elevation: 5,
+                  child: Column(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(
+                          8.0,
+                        ), // Rounded corners for image
+                        child: Image.asset(
+                          'assets/secondcar.jpg',
+                          height: 150,
+                          width: double.infinity,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        'COROLLA',
+                        style: TextStyle(
+                            color: Color.fromARGB(255, 7, 6, 110),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20),
+                      ),
+                      SizedBox(height: 4),
+                      Text(
+                        'Make: Toyota | Model: 2018 | Variant: Grande 1.8L',
+                        style: TextStyle(
+                            color: Color.fromARGB(255, 7, 6, 110),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 13),
+                      ),
+                      SizedBox(height: 8),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
